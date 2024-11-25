66b89cb39199059f8388558ef5445fdb87329c88
RockingWithTheBest <sikanweclyde@gmail.com>
2024-11-25 18:01:59 +0300

    feat : added and copleted LandLoadQuestion component

---
+++ Client-Side/src/Components/LandLordQuestion/LandLordQuestion.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ListAgents from '../AvailableAgents/ListAgents'
+import './LandLordQuestion.css';
+
+const LandLordQuestion =()=>{
+    return(
+        <div className="agentscoments">
+            <h2>No Spam Promise</h2>
+            <h3>Are you a landlord?</h3>
+            <p>Discover ways to increase your home's value and  get listed. No Spam.</p>
+            <div>
+                <ListAgents/>
+            </div>
+            <p>Join <span className="tenthousand">10,000+</span> other landlords in our estatery community.</p>
+        </div>
+    )
+}
+export default LandLordQuestion;
