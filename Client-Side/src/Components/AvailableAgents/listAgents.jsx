6407c5ca3f8fd36b457a3b1ff64d1c87cc7f4edb
RockingWithTheBest <sikanweclyde@gmail.com>
2024-11-25 17:57:57 +0300

    feat : available agents componets completed

---
+++ Client-Side/src/Components/AvailableAgents/listAgents.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import './listAgents.css';
+
+
+// import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Isaac from '../../assets/Agents/isaac.jpg';
+import Annie from '../../assets/Agents/annie.jpg';
+import Bennies from '../../assets/Agents/ben.jpg';
+
+export default function ListAgents() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src={Bennies} />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Bennies
+              </Typography>
+              {" — Location!! My properties are are situated in one of the most sought-after neigbhoods, offering excellent schools and amenities…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Travis Howard" src={Annie} />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Summer BBQ"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Annie Mwape
+              </Typography>
+              {" — Investment Potential!! My properties have signifacnt appreciation potential, making it a smart investment for your future…"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Cindy Baker" src={Isaac} />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Oui Oui"
+          secondary={
+            <React.Fragment>
+              <Typography
+                component="span"
+                variant="body2"
+                sx={{ color: 'text.primary', display: 'inline' }}
+              >
+                Isaac Chingeleshi
+              </Typography>
+              {' — Move-In Ready "You will love how well-maintained my properties are. They are truly ready move in!"…'}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
