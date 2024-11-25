66fc640ab0d93cfdcf7aa988deb2cf7a1ffc9359
RockingWithTheBest <sikanweclyde@gmail.com>
2024-11-25 18:07:12 +0300

    feat : MainPage page added

---
+++ Client-Side/src/Pages/MainPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../Components/Header/Header'
+import BodYOne  from '../Components/BodyOne/BodyOne';
+import Properties from '../Components/PropertyDisplay/properties'
+import SlideDisplay from '../Components/SlideDisplay/SlideDisplay'
+import LandLordQuestion from '../Components/LandLordQuestion/LandLordQuestion'
+import Footer from '../Components/Footer/footer';
+
+const Page=()=>{
+    return(
+        <div>
+           <Header/>
+           <BodYOne/> 
+           <Properties/>
+           <SlideDisplay/>  
+           <LandLordQuestion/>
+           <Footer/>
+        </div>
+    );
+};
+
+export default Page;
